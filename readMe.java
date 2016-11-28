66d99efc33669040627d17e93aa200528f9f4d58
Annankofi <fallingstar99@163.com>
2016-11-28 10:07:33 +0800

    modify readme

--- readMe.java
+++ readMe.java
@@ -2,9 +2,9 @@ Usage:
 1、setup a repo in gihub
 2、config your git bash
 3、git clone
-   git clone https://github.com/Annankofi/AnnanRepo.git
+   git clone https://github.com/Annankofi/HelloScript.git
    git remote rm origin
-   git remote add origin git@github.com:Annankofi/AnnanRepo.git 
+   git remote add origin git@github.com:Annankofi/HelloScript.git
 4、add your file
    note:ignore empty dir
 5、commit
